305a5110606102a4cd3fcaf8176b8c50d0ae7b9b
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2017-11-27 11:34:44 -0600

    update 6

---
+++ MadLibs/MadLibs/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listColor = New System.Windows.Forms.ListBox()
+        Me.listEst = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.listNumber = New System.Windows.Forms.ListBox()
+        Me.listBodyPart = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.listAnimal = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.listNoun = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.listPluralNoun = New System.Windows.Forms.ListBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'listColor
+        '
+        Me.listColor.FormattingEnabled = True
+        Me.listColor.Items.AddRange(New Object() {"Green", "Red", "Blue", "Yellow"})
+        Me.listColor.Location = New System.Drawing.Point(68, 69)
+        Me.listColor.Name = "listColor"
+        Me.listColor.Size = New System.Drawing.Size(120, 95)
+        Me.listColor.TabIndex = 0
+        '
+        'listEst
+        '
+        Me.listEst.FormattingEnabled = True
+        Me.listEst.Items.AddRange(New Object() {"biggest", "fastests", "fattes", "greatest", "shortest", "roundest", "smallest", "stealthiest", "unlovelist", "slenderest", "airworthiest", "seaworthiest", "uncommonest", "stubbornest"})
+        Me.listEst.Location = New System.Drawing.Point(214, 69)
+        Me.listEst.Name = "listEst"
+        Me.listEst.Size = New System.Drawing.Size(120, 95)
+        Me.listEst.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(65, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Color"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(211, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Word ending in est"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(372, 49)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Number"
+        '
+        'listNumber
+        '
+        Me.listNumber.FormattingEnabled = True
+        Me.listNumber.Items.AddRange(New Object() {"1", "10", "15", "100", "115", "55", "47", "81", "64", "75", "7.62"})
+        Me.listNumber.Location = New System.Drawing.Point(375, 69)
+        Me.listNumber.Name = "listNumber"
+        Me.listNumber.Size = New System.Drawing.Size(120, 95)
+        Me.listNumber.TabIndex = 5
+        '
+        'listBodyPart
+        '
+        Me.listBodyPart.FormattingEnabled = True
+        Me.listBodyPart.Items.AddRange(New Object() {"arms", "legs", "toes", "fingers", "heads", "belly buttons", "hips"})
+        Me.listBodyPart.Location = New System.Drawing.Point(68, 214)
+        Me.listBodyPart.Name = "listBodyPart"
+        Me.listBodyPart.Size = New System.Drawing.Size(120, 95)
+        Me.listBodyPart.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(65, 194)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Body Part plural"
+        '
+        'listAnimal
+        '
+        Me.listAnimal.FormattingEnabled = True
+        Me.listAnimal.Items.AddRange(New Object() {"dog", "cat", "bear", "turtle", "monkey", "raccoon"})
+        Me.listAnimal.Location = New System.Drawing.Point(214, 214)
+        Me.listAnimal.Name = "listAnimal"
+        Me.listAnimal.Size = New System.Drawing.Size(120, 95)
+        Me.listAnimal.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(211, 194)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Animal"
+        '
+        'listNoun
+        '
+        Me.listNoun.FormattingEnabled = True
+        Me.listNoun.Items.AddRange(New Object() {"house", "boat", "car", "city", "planet"})
+        Me.listNoun.Location = New System.Drawing.Point(375, 214)
+        Me.listNoun.Name = "listNoun"
+        Me.listNoun.Size = New System.Drawing.Size(120, 95)
+        Me.listNoun.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(372, 194)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(33, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Noun"
+        '
+        'listPluralNoun
+        '
+        Me.listPluralNoun.FormattingEnabled = True
+        Me.listPluralNoun.Items.AddRange(New Object() {"house", "desks", "trees", "footballs"})
+        Me.listPluralNoun.Location = New System.Drawing.Point(68, 368)
+        Me.listPluralNoun.Name = "listPluralNoun"
+        Me.listPluralNoun.Size = New System.Drawing.Size(120, 95)
+        Me.listPluralNoun.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(65, 348)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Label7"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(278, 368)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 14
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(278, 424)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 15
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(435, 368)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 16
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(597, 528)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.listPluralNoun)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.listNoun)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.listAnimal)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.listBodyPart)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.listNumber)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.listEst)
+        Me.Controls.Add(Me.listColor)
+        Me.Name = "Form1"
+        Me.Text = "MadLibs"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents listColor As System.Windows.Forms.ListBox
+    Friend WithEvents listEst As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents listNumber As System.Windows.Forms.ListBox
+    Friend WithEvents listBodyPart As System.Windows.Forms.ListBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents listAnimal As System.Windows.Forms.ListBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents listNoun As System.Windows.Forms.ListBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents listPluralNoun As System.Windows.Forms.ListBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
